e1bec05d1b7fcda3c7cea8e7e0b8d360cd6f305a
Hanna Wingaard <hanna.wingaard@gmail.com>
2016-03-09 15:09:53 +0100

    no message

---
+++ src/tester/testSkilt.java
@@ -0,0 +1,31 @@
+package tester;
+
+import javafx.scene.image.Image;
+import java.io.ByteArrayInputStream;
+import klasser.Skilt;
+import klasser.SkiltFart;
+import klasser.Skilt_med_tekst;
+import klasser.Skilt_uten_tekst;
+
+public class testSkilt extends junit.framework.TestCase {
+	
+	public void testSkiltFart(){
+		SkiltFart skilt = new SkiltFart(110);
+		assertEquals (110, skilt.getSkiltnr());
+		assertEquals (1, skilt.getSkiltgruppe());
+	}
+
+	public void testSkilt_med_tekst(){
+		Skilt_med_tekst skilt = new Skilt_med_tekst(1, "Svært stor elgfare");
+		assertEquals (1, skilt.getSkiltnr());
+		assertEquals (2, skilt.getSkiltgruppe());
+		assertEquals ("Svært stor elgfare", skilt.hentTekst());
+	}
+	
+	public void testSkilt_uten_tekst(){
+		Skilt_uten_tekst skilt = new Skilt_uten_tekst(8);
+		assertEquals (8, skilt.getSkiltnr());
+		assertEquals (3, skilt.getSkiltgruppe());
+	}
+	
+}
