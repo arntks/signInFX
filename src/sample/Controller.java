16a0c592f1b7a53dfbf9bc6c10d020a618d9103a
arntks <krizzi230@gmail.com>
2016-03-09 11:30:24 +0100

    ordnet litt i view

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,14 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML TextField text;
+
+    @FXML public void knapp() {
+
+    }
+
 }
