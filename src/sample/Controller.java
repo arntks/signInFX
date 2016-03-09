bf8e8a00f4ceac53e5e1709a19723cb95a002358
Martin Vee <martinvee@hotmail.com>
2016-03-09 13:11:45 +0100

    Sample-test

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,7 +2,16 @@ package sample;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class Controller {
-
+	
+	@FXML
+    private ImageView hovedBilde1;
+	
+	public void setImage() throws Exception{
+        Image image = new Image("img/1_110.jpg");
+        hovedBilde1.setImage(image);
+	}
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,20 +1,37 @@
 package sample;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+	
+	
+	
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 600, 337.5));
+        Controller c = new Controller();
+        c.setImage();
+        
+
+        
+        
+        
+        
         primaryStage.show();
     }
+    
+
 
 
     public static void main(String[] args) {
