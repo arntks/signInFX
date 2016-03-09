87d176d2e040ad596c6687e67b5f4cfffebfe4e9
Synne Fog <synne.fog@teknologiporten.no>
2016-03-09 14:35:48 +0100

    testGetfile

--- src/tester/testSplitt.java
+++ src/tester/testSplitt.java
@@ -1,11 +1,15 @@
 package tester;
 
+import java.io.File;
+
 import klasser.Splitt;
 
 public class testSplitt extends junit.framework.TestCase{
 	
 	public void testDele(){
-		
+		Splitt string = new Splitt("pakkeTest.txt");
+		File fil = new File ("pakkeTest.txt");
+		assertEquals(fil, string.getFile());
 	}
 
 }
