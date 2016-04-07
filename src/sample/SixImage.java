6e9e68bd76f4a2aab9bff6a2b27a8c7dc85f848c
Anita Bø <anitaboe@dhcp-10-22-73-98.wlan.ntnu.no>
2016-04-07 14:50:07 +0200

    Oppdatering av skiltpakker og siximage-klasen

--- src/sample/SixImage.java
+++ src/sample/SixImage.java
@@ -21,7 +21,7 @@ public class SixImage {
 	private ArrayList <String> textList = new ArrayList <String>();
 	
 	public SixImage(ArrayList<Skilt> skiltGruppe, ArrayList<ImageView> pictures, ArrayList<Label> labels){
-		numList.addAll(Arrays.asList(0,100,150,220,300,400));
+		numList.addAll(Arrays.asList(60,60,302,468,302,468));
 		for(int i=0; i<6; i++){
 			imgList.add(skiltGruppe.get(i).hentBilde());
 			textList.add(skiltGruppe.get(i).hentTekst());
@@ -35,17 +35,28 @@ public class SixImage {
 	public void makeP(){
 		int h;
 		int b;
-		int y=10;
+		int y;
 		
 		for(int i=0; i<6; i++){
 			pictures.get(i).setImage(imgList.get(i));
-			if(i<= 1){
-				h=100;
+			if(i==0){
+				h=150;
+				b=150;
+				y=60;
+			}else if(i==1){
+				h=150;
+				b=150;
+				y=230;
+			}else if((i==2)||(i==3)){
+				h=87;
 				b=100;
+				y=173;
 			}else{
-				h=50;
-				b=50;
+				h=87;
+				b=100;
+				y=283;
 			}
+		
 			pictures.get(i).setFitHeight(h);
 			pictures.get(i).setFitWidth(b);
 			pictures.get(i).setX(numList.get(i));
@@ -59,9 +70,14 @@ public class SixImage {
 	}
 	
 	public void makeT(){
-		int y=170;
+		int y;
 		
 		for(int i=2; i<6; i++){
+			if(i==2||i==3){
+				y=259;
+			}else{
+				y=369;
+			}
 			
 			labels.get(i-2).setPrefSize(150, 15);
 			labels.get(i-2).setText(textList.get(i));
